4e43cb9abed5d2e08a1e99b7e6d4c0275b9c6992
Miguel Juarez <mfjuaarez@gmail.com>
2023-02-11 16:55:25 -0300

    Se agrego el ejercicio de clases y objetos

---
+++ ConsoleApp10-clases_y_objetos/ConsoleApp10-clases_y_objetos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ Confeccionar una clase que represente un empleado.
+ Definir como atributos su nombre y su sueldo. 
+ Confeccionar los métodos para la carga, otro para imprimir 
+ sus datos y por último uno que imprima un mensaje si debe 
+ pagar impuestos (si el sueldo supera a 3000)
+ */
+
+
+
+
+
+
+
+namespace ConsoleApp10_clases_y_objetos
+{
+    internal class Empleado
+    {
+        private string nombre;
+        private int sueldo;
+
+        public void Inicializar()
+        {
+            Console.WriteLine("ingrese el nombre del empleado");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese el sueldo");
+            sueldo = int.Parse(Console.ReadLine());
+        }
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("datos del empleado");
+            Console.WriteLine("nombre: " + nombre + " ; sueldo: " + sueldo);
+        }
+
+        public void Impuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("Debe pagar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("No debe pagar impuestos");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado();
+            empleado1.Inicializar();    
+            empleado1.ImprimirDatos();
+            empleado1.Impuestos();
+            Console.ReadKey();
+        }
+    }
+}
