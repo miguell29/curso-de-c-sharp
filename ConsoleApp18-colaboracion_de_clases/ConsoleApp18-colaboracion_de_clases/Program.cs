3d3c4df98d37c41b33a9559657348b1168cbf885
Miguel Juarez <mfjuaarez@gmail.com>
2023-03-06 13:57:08 -0300

    Se completo el ejercicio de colaboracion de clases

---
+++ ConsoleApp18-colaboracion_de_clases/ConsoleApp18-colaboracion_de_clases/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18_colaboracion_de_clases
+{
+    /*
+     * Plantear una clase Club y otra clase Socio.
+     * La clase Socio debe tener los siguientes atributos privados:
+     * nombre y la antigüedad en el club (en años). 
+     * En el constructor pedir la carga del nombre y su antigüedad. 
+     * La clase Club debe tener como atributos 3 objetos de la clase Socio.
+     * Definir una responsabilidad para imprimir el nombre del socio con mayor antigüedad en el club.
+     */
+        public class Socio
+        {
+            private string nombre;
+            private  int antiguedad;
+
+            public Socio()
+            {
+                Console.WriteLine("Ingrese el nombre del socio");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese la antiguedad del socio");
+                antiguedad = int.Parse(Console.ReadLine());
+            }
+            public int Antigue()
+            {
+            return antiguedad;
+            }
+            public void Imprimir()
+            {
+                Console.WriteLine($"Nombre: {nombre} antiguedad: {antiguedad}");
+            }
+        }
+
+
+    internal class Club
+    {
+        private Socio socio1,socio2,socio3;
+        Club()
+        {
+            socio1 = new Socio();
+            socio2 = new Socio();
+            socio3 = new Socio();
+        }
+            
+        public void SocioAntiguo()
+        {
+            Console.WriteLine("Socio mas Antiguo: ");
+            if (socio1.Antigue() > socio2.Antigue() && socio1.Antigue() > socio3.Antigue()) 
+            {
+                socio1.Imprimir();
+            }
+            else
+            {
+                if (socio2.Antigue() > socio1.Antigue() && socio2.Antigue() > socio3.Antigue())
+                {
+                    socio2.Imprimir();
+                }
+                else
+                {
+                    socio3.Imprimir();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+           Club club = new Club();
+            club.SocioAntiguo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp18-colaboracion_de_clases/ConsoleApp18-colaboracion_de_clases/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsoleApp18-colaboracion_de_clases")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp18-colaboracion_de_clases")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("0e55faf0-8fca-4161-9459-f4048ceb364b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
